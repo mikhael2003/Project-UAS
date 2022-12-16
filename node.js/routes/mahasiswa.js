76ec7f2e094629786a5319bc8f7973a695d87f08
mikhael <mikhaelsiman2003@gmail.com>
2022-12-16 16:08:49 +0700

    update file js

---
+++ node.js/routes/mahasiswa.js
@@ -0,0 +1,34 @@
+// (5) Buat router Mahasiswa
+const express = require('express')
+const router = express.Router() 
+const Mahasiswa = require('../models/Mahasiswa')
+
+// Create 
+router.post('/', async(req, res) => {
+    // tampung input mahasiswa 
+    const mahasiswaPost = new Mahasiswa({
+        nama: req.body.nama,
+        alamat: req.body.alamat
+    })
+
+    try {
+        // simpan data 
+        const mahasiswa = await mahasiswaPost.save()
+        // response
+        res.json(mahasiswa)
+    } catch (error) {
+        res.json({message: error})
+    }
+})
+
+// Read
+router.get('/', async(req, res) => {
+    try {
+        const mahasiswa = await Mahasiswa.find()
+        res.json(mahasiswa)
+    } catch (error) {
+        res.json({message: error})
+    }
+})
+
+module.exports = router
